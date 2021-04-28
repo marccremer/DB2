74a06d356aa1b5df9d3b6335d20579dfe1aadee8
verxes <marc@cremer.dev>
2021-04-28 16:00:59 +0200

    better init migration

--- migrations/20201211140910_create_free_tables.js
+++ migrations/20201211140910_create_free_tables.js
@@ -6,20 +6,47 @@ const { idprime } = require('../src/tableUtils');
  */
 
 exports.up = async (knex) => {
+  await knex.schema.createTable(tableNames.table, (table) => {
+    table.increments().notNullable();
+  });
+  
+    await knex.schema.createTable(tableNames.tischgruppe, (table) => {
+      table.increments().notNullable();
+    });
+  
   await knex.schema.createTable(tableNames.raum, (table) => {
     table.increments().notNullable();
     table.string('RaumName', 20).notNullable();
     table.float('Fläche in m2')
   });
+
+  await knex.schema.createTable(tableNames.kontakt, (table) => {
+    table.increments().notNullable();
+  });
+
   await knex.schema.createTable(tableNames.adresse, (table) => {
     table.increments().notNullable();
-    table.string('Stadtname', 105).notNullable();
-    table.string('Flughafen', 100).notNullable();
-    table.string('Land', 60).notNullable();
+  });
+  
+  await knex.schema.createTable(tableNames.kunde, (table) => {
+    table.increments().notNullable();
+  });
+  
+  await knex.schema.createTable(tableNames.reservierung, (table) => {
+    table.increments().notNullable();
   });
 };
 
 exports.down = async (knex) => {
-  await knex.schema.dropTable(tableNames.kunde);
-  await knex.schema.dropTable(tableNames.stadt);
+  await Promise.all(
+    [
+      tableNames.table,
+      tableNames.tischgruppe,
+      tableNames.raum,
+      tableNames.adresse,
+      tableNames.kontakt,
+      tableNames.kunde,
+      tableNames.reservierung,
+    ].map((tableName) => knex.schema.dropTableIfExists(tableName))
+  );
 };
--- seeds/init.js
+++ seeds/init.js
@@ -5,26 +5,5 @@ const tableNames = require('../src/tableNames');
 exports.seed = async (knex) => {
   // Deletes ALL existing entries
   await Promise.all(Object.values(tableNames).map((name) => knex(name).del()));
-  const kunde1 = {
-    Vorname: 'Marc',
-    Nachnahme: 'Cremer',
-    Adresse: 'Auf dem Teich 10',
-  };
-  const [createdKunde] = await knex(tableNames.kunde)
-    .insert(kunde1)
-    .returning('*');
 
-  const stadt1 = {
-    Stadtname: 'Gummersbach',
-    Flughafen: 'Gummersbach Flughafen',
-    Land: 'Deutschland',
-  };
-  const [createdStadt] = await knex(tableNames.stadt)
-    .insert(stadt1)
-    .returning('*');
-  await knex(tableNames.stadt).insert({
-    Stadtname: 'Madrid',
-    Flughafen: 'Adolfo Suárez Madrid–Barajas Flughafen',
-    Land: 'Spanien',
-  });
 };
