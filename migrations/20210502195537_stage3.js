c67cd0fd7372be6523db99609755b5156f04a217
verxes <marc@cremer.dev>
2021-05-02 21:18:15 +0200

    finale relationen

---
+++ migrations/20210502195537_stage3.js
@@ -0,0 +1,43 @@
+const tableNames = require('../src/tableNames');
+const {
+  addDefaultColumns,
+  createNameTable,
+  url,
+  email,
+  references,
+} = require('../src/tableUtils.js');
+
+/**
+ * @param {import('knex')} knex
+ */
+exports.up = async (knex) => {
+  await knex.schema.createTable(tableNames.reservierung, (table) => {
+    table.increments().notNullable();
+    table.date('Datum').notNullable
+    references(table,tableNames.kunde)
+  });
+
+  await knex.schema.createTable(tableNames.tisch, (table) => {
+    table.increments().notNullable();
+    references(table,tableNames.tischgruppe)
+  });
+
+  await knex.schema.createTable(tableNames.gebuchterTisch, (table) => {
+    references(table,tableNames.tisch)
+    references(table,tableNames.reservierung,true,'reservierung')
+    table.primary('reservierung_id')
+  });
+
+};
+
+exports.down = async (knex) => {
+  await Promise.all(
+    [
+      tableNames.reservierung,
+      tableNames.tisch,
+      tableNames.gebuchterTisch
+    ]
+    .reverse()
+    .map((tableName) => knex.schema.dropTableIfExists(tableName))
+  );
+};
