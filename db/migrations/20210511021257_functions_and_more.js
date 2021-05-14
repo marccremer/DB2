b94363bd1494188dfa630fcafc21a92357ea3614
verxes <marc@cremer.dev>
2021-05-14 23:02:00 +0200

    add function migreation

--- db/migrations/20210511021257_functions_and_more.js
+++ db/migrations/20210511021257_functions_and_more.js
@@ -1,17 +1,34 @@
-
 /**
  * @param {import('knex')} knex
  */
 exports.up = async (knex) => {
-  await knex.raw(`create
-  function Hello() returns char(36)
-    BEGIN
-      RETURN 'Hello';
-    END;
-`)
+  await knex.raw('DROP FUNCTION IF EXISTS Hello');
+  await knex.raw('DROP FUNCTION IF EXISTS isEligible');
+  await knex.raw(
+    `create
+      function Hello() returns char(36)
+        BEGIN
+          RETURN 'Hello';
+        END;
+    `);
+
+  await knex.raw(
+    `CREATE FUNCTION isEligible(
+      age INTEGER
+      )
+      RETURNS VARCHAR(20)
+      DETERMINISTIC
+      BEGIN
+      IF age > 18 THEN
+      RETURN ("yes");
+      ELSE
+      RETURN ("No");
+      END IF;
+      END;`)
 
 };
 
 exports.down = async (knex) => {
-  await knex.raw('DROP FUNCTION IF EXISTS Hello')
+  knex.raw('DROP FUNCTION IF EXISTS Hello')
+  knex.raw('DROP FUNCTION IF EXISTS isEligible')
 };
