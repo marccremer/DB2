397d37026bf0472caa6287c2fdc6d1d0bd3a08ba
edgarKinetz98 <83030210+edgarKinetz98@users.noreply.github.com>
2021-05-24 20:37:59 +0200

    added new procedure

--- db/migrations/20210511021257_functions_and_more.js
+++ db/migrations/20210511021257_functions_and_more.js
@@ -12,7 +12,11 @@ exports.up = async (knex) => {
         END;
     `);
 
-  await knex.raw(
+    await knex.raw('DROP FUNCTION IF EXISTS Hello')
+    await knex.raw('DROP FUNCTION IF EXISTS isEligible')
+    await knex.raw('DROP PROCEDURE IF EXISTS rebooking')
+    await knex.raw('DROP TRIGGER IF EXISTS reduzierung_plaetze')
+    await knex.raw(
     `CREATE FUNCTION isEligible(
       age INTEGER
       )
@@ -26,9 +30,37 @@ exports.up = async (knex) => {
       END IF;
       END;`)
 
+    
+
+        await knex.raw(
+
+    
+          `
+          
+          DELIMITER // 
+          CREATE PROCEDURE rebooking(newDate DATE, bookingID INT)
+          BEGIN 
+          DECLARE v_date DATE;
+          SELECT Datum INTO v_date FROM Reservierung WHERE id = bookingID;
+          IF v_date = CURDATE() AND v_date < newDate THEN
+              UPDATE Reservierung
+              SET Datum  = newDate
+              WHERE id = bookingID;
+          ELSE 
+          SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Umbuchung nur für den Folgetag möglich!';
+
+          
+          END IF;    
+      
+          END // 
+          DELIMITER ;
+          `
+        );
+
 };
 
 exports.down = async (knex) => {
   knex.raw('DROP FUNCTION IF EXISTS Hello')
   knex.raw('DROP FUNCTION IF EXISTS isEligible')
+  knex.raw('DROP PROCEDURE IF EXISTS rebooking')
 };
