1a73b53eea2d52a1019cdffc94b862ed4f20b383
verxes <marc@cremer.dev>
2021-05-27 19:16:30 +0200

    remove old tables

--- src/tableNames.js
+++ src/tableNames.js
@@ -5,8 +5,6 @@ module.exports = {
   reservierung: 'Reservierung',
   gebuchterTisch: 'Tischreservierung',
   begleiter: 'Begleiter',
-  teilnehmer: 'Teilnehmer',
-  reservierer: 'Reservierer',
   kunde: 'Kunde',
   kontaktdaten: 'Kontaktdaten',
   adresse: 'Adresse',
