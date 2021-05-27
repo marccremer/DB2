f16ef557f24fc9fbd048cb8d22d7c83664ccc788
verxes <marc@cremer.dev>
2021-05-27 19:10:29 +0200

    better split of FK constraints

--- db/migrations/20210502195537_stage3.js
+++ db/migrations/20210502195537_stage3.js
@@ -5,27 +5,17 @@ const tableNames = require('../../src/tableNames');
  * @param {import('knex')} knex
  */
 exports.up = async (knex) => {
-  await knex.schema.createTable(tableNames.begleiter, (table) => {
-    references(table, tableNames.kunde);
-  });
-
-  await knex.schema.alterTable(tableNames.begleiter, (table) => {
-    table.primary(`${tableNames.kunde}_id`);
-  });
-
-  await knex.schema.createTable(tableNames.reservierer, (table) => {
-    references(table, tableNames.kunde);
-    table.integer('Kreditkartennummer');
-  });
-
-  await knex.schema.alterTable(tableNames.reservierer, (table) => {
-    table.primary(`${tableNames.kunde}_id`);
-  });
 
   await knex.schema.createTable(tableNames.reservierung, (table) => {
     table.increments().notNullable();
     table.datetime('Datumszeit').notNullable();
     table.boolean('deleted').defaultTo(false);
+    references(table,tableNames.kunde,true,"reservierer_id");
+  });
+
+  await knex.schema.createTable(tableNames.begleiter, (table) => {
+    references(table, tableNames.kunde);
+    references(table,tableNames.reservierung);
   });
 
   await knex.schema.createTable(tableNames.gebuchterTisch, (table) => {
@@ -34,28 +24,14 @@ exports.up = async (knex) => {
     table.primary('reservierung_id');
   });
 
-  await knex.schema.createTable(tableNames.teilnehmer, (table) => {
-    references(table, tableNames.reservierung);
-    table.integer(`${tableNames.begleiter}_id`).unsigned()
-      .references(`${tableNames.kunde}_id`)
-      .inTable(tableNames.begleiter)
-      .onDelete('cascade');
-    table.integer(`${tableNames.reservierer}_id`).unsigned()
-      .references(`${tableNames.kunde}_id`)
-      .inTable(tableNames.reservierer)
-      .onDelete('cascade');
-    table.primary([`${tableNames.reservierer}_id`, `${tableNames.reservierung}_id`]);
-  });
 };
 
 exports.down = async (knex) => {
   await Promise.all(
     [
-      tableNames.begleiter,
-      tableNames.reservierer,
       tableNames.reservierung,
+      tableNames.begleiter,
       tableNames.gebuchterTisch,
-      tableNames.teilnehmer,
     ]
       .reverse()
       .map((tableName) => knex.schema.dropTableIfExists(tableName)),
