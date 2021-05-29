461dc5a7df2892c837de118ff8f9eb15d7146d57
verxes <marc@cremer.dev>
2021-05-29 12:59:46 +0200

    added begleiter

--- docs/sqlcommands/oraclsql/generateNewSchema.sql
+++ docs/sqlcommands/oraclsql/generateNewSchema.sql
@@ -24,3 +24,23 @@ BEGIN
   INTO   :new.ID
   FROM   dual;
 END;
+
+create type begleiter_t under kunden_t (
+    stammkunde number
+                                       )final ;
+create table begleiter of begleiter_t;
+
+alter table begleiter add constraint pk_begleiter primary key (ID);
+alter table begleiter add constraint fk_bge_kontakt
+    foreign key(KONTAKTDATEN_ID) references "Kontaktdaten"("id");
+alter table begleiter add constraint fk_begleiter
+    foreign key(RESERVERINGS_ID) references "Reservierung"("id");
+create sequence begleiter_seq;
+CREATE OR REPLACE TRIGGER BEGLEITER_autoinc
+BEFORE INSERT ON BEGLEITER
+FOR EACH ROW
+BEGIN
+  SELECT begleiter_seq.NEXTVAL
+  INTO   :new.ID
+  FROM   dual;
+END;
