40feae2681fa5f5ea0b6d6921e9b7e834326802d
maikLoewenML <loewenmaik@gmail.com>
2021-05-30 14:02:58 +0200

    Oracle Schema

--- docs/sqlcommands/oraclsql/generateNewSchema.sql
+++ docs/sqlcommands/oraclsql/generateNewSchema.sql
@@ -1,51 +1,3 @@
-create type kunden_t as Object
-(
-    ID           NUMBER,
-    "Vorname"       varchar(200),
-    "Nachname"      varchar(200),
-    Kontaktdaten_id number
-) not  final;
-create type reservier_t under kunden_t
-( "Kreditkartennummer" varchar(200)) final;
-
-
-create table reservier of reservier_t;
-alter table reservier add constraint fk_kontakt
-
-    foreign key(KONTAKTDATEN_ID) references "Kontaktdaten"("id");
-alter table RESERVIER add constraint pk_prices
-    primary key(ID);
-create sequence reservier_seq;
-CREATE OR REPLACE TRIGGER reservier_autoinc
-BEFORE INSERT ON RESERVIER
-FOR EACH ROW
-BEGIN
-  SELECT reservier_seq.NEXTVAL
-  INTO   :new.ID
-  FROM   dual;
-END;
-
-create type begleiter_t under kunden_t (
-    stammkunde number
-                                       )final ;
-create table begleiter of begleiter_t;
-
-alter table begleiter add constraint pk_begleiter primary key (ID);
-alter table begleiter add constraint fk_bge_kontakt
-    foreign key(KONTAKTDATEN_ID) references "Kontaktdaten"("id");
-alter table begleiter add constraint fk_begleiter
-    foreign key(RESERVERINGS_ID) references "Reservierung"("id");
-create sequence begleiter_seq;
-CREATE OR REPLACE TRIGGER BEGLEITER_autoinc
-BEFORE INSERT ON BEGLEITER
-FOR EACH ROW
-BEGIN
-  SELECT begleiter_seq.NEXTVAL
-  INTO   :new.ID
-  FROM   dual;
-END;
-
-
 --Tabelle Raum
 create table Raum
 (
@@ -95,7 +47,7 @@ create table Kontaktdaten
 (
     id            number(10) check (id > 0) not null primary key,
     Adresse_id    number(10) check (Adresse_id > 0) not null,
-    E-Mail        varchar2(254),
+    "E-Mail"       varchar(254),
     Telefonnummer number(10)
 );
 
@@ -178,6 +130,112 @@ end;
 /
 alter table Tisch
     add constraint tisch_tischgruppe_id_foreign foreign key (Tischgruppe_id) references Tischgruppe (id) on delete cascade;
+
+
+
+create type kunden_t as Object
+(
+    ID           NUMBER,
+    "Vorname"       varchar(200),
+    "Nachname"      varchar(200),
+    Kontaktdaten_id number
+) not  final;
+create type reservier_t under kunden_t
+( "Kreditkartennummer" varchar(200)) final;
+
+
+create table reservier of reservier_t;
+alter table reservier add constraint fk_kontakt
+
+    foreign key(KONTAKTDATEN_ID) references "KONTAKTDATEN"("ID");
+alter table RESERVIER add constraint pk_prices
+    primary key(ID);
+create sequence reservier_seq;
+CREATE OR REPLACE TRIGGER reservier_autoinc
+BEFORE INSERT ON RESERVIER
+FOR EACH ROW
+BEGIN
+  SELECT reservier_seq.NEXTVAL
+  INTO   :new.ID
+  FROM   dual;
+END;
+
+
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table Reservierung
+(
+    id                number(10) check (id > 0) not null primary key,
+    Datumszeit        timestamp(0)     not null,
+    deleted           char(1) default '0',
+    storniert         char(1) default '0',
+    reservierer_id number(10) check (reservierer_id > 0) not null
+);
+
+-- Generate ID using sequence and trigger
+create sequence Reservierung_seq start with 1 increment by 1;
+
+create or replace trigger Reservierung_seq_tr
+ before insert on Reservierung for each row
+ when (new.id is null)
+begin
+ select Reservierung_seq.nextval into :new.id from dual;
+end;
+/
+alter table Reservierung
+    add constraint reservierung_reservierer_id_id_foreign foreign key (reservierer_id) references RESERVIER(id) on delete cascade;
+
+
+
+
+create type begleiter_t under kunden_t (
+    stammkunde number,
+    reservierungs_id number
+                                       )final ;
+create table begleiter of begleiter_t;
+
+alter table begleiter add constraint pk_begleiter primary key (ID);
+alter table begleiter add constraint fk_bge_kontakt
+    foreign key(KONTAKTDATEN_ID) references "KONTAKTDATEN"("ID");
+alter table begleiter add constraint fk_reservierungen
+    foreign key(reservierungs_id) references "RESERVIERUNG"("ID");
+create sequence begleiter_seq;
+CREATE OR REPLACE TRIGGER BEGLEITER_autoinc
+BEFORE INSERT ON BEGLEITER
+FOR EACH ROW
+BEGIN
+  SELECT begleiter_seq.NEXTVAL
+  INTO   :new.ID
+  FROM   dual;
+END;
+
+
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+create table Tischreservierung
+(
+    Tisch_id        number(10) check (Tisch_id > 0) not null,
+    reservierung_id number(10) check (reservierung_id > 0) not null
+);
+
+alter table Tischreservierung
+    add constraint tischreservierung_tisch_id_foreign foreign key (Tisch_id) references Tisch (id) on delete cascade;
+alter table Tischreservierung
+    add constraint tischreservierung_reservierung_id_foreign foreign key (reservierung_id) references Reservierung (id) on delete cascade;
+alter table Tischreservierung
+    add constraint Tischreservierung_pkey primary key (reservierung_id, Tisch_id);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
